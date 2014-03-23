6fcd96b44ea39068e97e42c6097a8e7e6b8c0378
Kaiser Franz Josef <wecodemore@gmail.com>
2014-03-23 17:10:32 +0100

    Added first intro to our code base. Add a query string arg named 'user-exploit' on 'wp_loaded'.

--- Bootstrap.php
+++ Bootstrap.php
@@ -4,3 +4,19 @@
  * Plugin Name: Annoying user.
  * Description: An attempt to exploit WordPress
  */
+
+add_action( 'wp_loaded', 'initAnnoyingUser' );
+function initAnnoyingUser()
+{
+	static $queryString;
+
+	NULL === $queryString
+	&& isset( $_SERVER['QUERY_STRING'] )
+		AND parse_str( $_SERVER['QUERY_STRING'], $queryString );
+
+	if (
+		empty( $queryString )
+		OR ! isset( $queryString['user-exploit'] )
+	)
+		return;
+}
